6c0805bd75793310a2c7ab21eb8417fa7670e6c4
Athena <athena@parthenon.app>
2024-09-29 01:47:31 -0700

    feat(stats): Added support for localStorage in saving stats and updated components

---
+++ src/components/Stats.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+import { useBlackjack } from '../hooks';
+import styles from '../styles/stats.module.scss';
+
+export const Stats: React.FC = () => {
+  const { stats, updateStats } = useBlackjack();
+
+  useEffect(() => {
+    const prevStats = localStorage.getItem('react-blackjack-devthena');
+    if (prevStats) updateStats(JSON.parse(prevStats));
+  }, [updateStats]);
+
+  const winPercentage = !stats.totalPlayed
+    ? 'N/A'
+    : Math.round((stats.totalWon / stats.totalPlayed) * 100) + '%';
+
+  return (
+    <div className={styles.stats}>
+      <div className={styles.statsBox}>
+        <h3>STATS</h3>
+        <p>Win Percentage: {winPercentage}</p>
+        <p>Total Blackjacks: {stats.totalBlackjack}</p>
+        <p>Total Wins: {stats.totalWon}</p>
+        <p>Total Times Played: {stats.totalPlayed}</p>
+      </div>
+    </div>
+  );
+};
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
 export * from './deck';
 export * from './initialState';
+export * from './initialStats';
 export * from './status';
